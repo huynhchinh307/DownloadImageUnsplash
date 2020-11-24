9a075c9c17bcde0ca1c64b04927daed46bbd9d92
huynhchinh307 <huynhchinh307@gmail.com>
2020-11-24 17:18:12 +0700

    commit

--- DownloadMuti/RadForm1.Designer.cs
+++ DownloadMuti/RadForm1.Designer.cs
@@ -28,20 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RadForm1));
+            this.btnStart = new Telerik.WinControls.UI.RadButton();
+            this.office2010BlueTheme1 = new Telerik.WinControls.Themes.Office2010BlueTheme();
+            this.st = new Telerik.WinControls.UI.RadStatusStrip();
+            this.lblStatus = new Telerik.WinControls.UI.RadLabelElement();
+            this.txtNum = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.btn_SetFolder = new Telerik.WinControls.UI.RadButton();
+            this.ckbTopic = new System.Windows.Forms.ComboBox();
+            this.ckb_MoTa = new Telerik.WinControls.UI.RadCheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.st)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_SetFolder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ckb_MoTa)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(38, 90);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(202, 24);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Tiến hành tải xuống";
+            this.btnStart.ThemeName = "Office2010Blue";
+            this.btnStart.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // st
+            // 
+            this.st.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.lblStatus});
+            this.st.Location = new System.Drawing.Point(0, 158);
+            this.st.Name = "st";
+            this.st.Size = new System.Drawing.Size(294, 21);
+            this.st.TabIndex = 2;
+            this.st.ThemeName = "Office2010Blue";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.st.SetSpring(this.lblStatus, false);
+            this.lblStatus.Text = "Trạng thái";
+            this.lblStatus.TextWrap = true;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Enabled = false;
+            this.txtNum.Location = new System.Drawing.Point(119, 12);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(121, 20);
+            this.txtNum.TabIndex = 3;
+            this.txtNum.Text = "1000";
+            this.txtNum.ThemeName = "Office2010Blue";
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Location = new System.Drawing.Point(12, 12);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(90, 18);
+            this.radLabel1.TabIndex = 4;
+            this.radLabel1.Text = "Số lượng cần tải:";
+            this.radLabel1.ThemeName = "Office2010Blue";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Location = new System.Drawing.Point(12, 36);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(44, 18);
+            this.radLabel2.TabIndex = 5;
+            this.radLabel2.Text = "Chủ đề:";
+            this.radLabel2.ThemeName = "Office2010Blue";
+            // 
+            // btn_SetFolder
+            // 
+            this.btn_SetFolder.Location = new System.Drawing.Point(12, 120);
+            this.btn_SetFolder.Name = "btn_SetFolder";
+            this.btn_SetFolder.Size = new System.Drawing.Size(270, 25);
+            this.btn_SetFolder.TabIndex = 8;
+            this.btn_SetFolder.Text = "Chọn thư mục lưu";
+            this.btn_SetFolder.ThemeName = "Office2010Blue";
+            this.btn_SetFolder.Click += new System.EventHandler(this.btn_SetFolder_Click);
+            // 
+            // ckbTopic
+            // 
+            this.ckbTopic.FormattingEnabled = true;
+            this.ckbTopic.Items.AddRange(new object[] {
+            "Không chủ đề",
+            "Kiến trúc",
+            "Thể thao",
+            "Du lịch",
+            "Làm đẹp",
+            "Thời trang",
+            "Vật nuôi",
+            "Nghệ thuật",
+            "Nhiếp ảnh",
+            "Ẩm thực"});
+            this.ckbTopic.Location = new System.Drawing.Point(119, 39);
+            this.ckbTopic.Name = "ckbTopic";
+            this.ckbTopic.Size = new System.Drawing.Size(121, 21);
+            this.ckbTopic.TabIndex = 9;
+            // 
+            // ckb_MoTa
+            // 
+            this.ckb_MoTa.Location = new System.Drawing.Point(119, 66);
+            this.ckb_MoTa.Name = "ckb_MoTa";
+            this.ckb_MoTa.Size = new System.Drawing.Size(91, 18);
+            this.ckb_MoTa.TabIndex = 10;
+            this.ckb_MoTa.Text = "Tải kèm mô tả";
+            // 
             // RadForm1
-            //
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 179);
+            this.Controls.Add(this.ckb_MoTa);
+            this.Controls.Add(this.ckbTopic);
+            this.Controls.Add(this.btn_SetFolder);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.st);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RadForm1";
-            this.Text = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "Download Muti Support";
+            this.ThemeName = "Office2010Blue";
+            this.Load += new System.EventHandler(this.RadForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.btnStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.st)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_SetFolder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ckb_MoTa)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Telerik.WinControls.UI.RadButton btnStart;
+        private Telerik.WinControls.Themes.Office2010BlueTheme office2010BlueTheme1;
+        private Telerik.WinControls.UI.RadStatusStrip st;
+        private Telerik.WinControls.UI.RadTextBox txtNum;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadButton btn_SetFolder;
+        private System.Windows.Forms.ComboBox ckbTopic;
+        private Telerik.WinControls.UI.RadCheckBox ckb_MoTa;
+        private Telerik.WinControls.UI.RadLabelElement lblStatus;
     }
 }
--- DownloadMuti/RadForm1.cs
+++ DownloadMuti/RadForm1.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using DownloadMuti.Properties;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Telerik.WinControls;
 using Telerik.WinControls.UI;
@@ -13,9 +20,208 @@ namespace DownloadMuti
 {
     public partial class RadForm1 : Telerik.WinControls.UI.RadForm
     {
+        private string path = null;
+
         public RadForm1()
         {
             InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string[] api = File.ReadAllLines("api.txt");
+            int number;
+            int key = 0;
+            if (path == null)
+            {
+                MessageBox.Show("Chưa chọn thư mục lưu hình.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (int.TryParse(txtNum.Text, out number))
+            {
+                int num = int.Parse(txtNum.Text);
+                IRestResponse response = null;
+                string topic = this.ckbTopic.SelectedValue.ToString();
+                this.lblStatus.Text = "Đang lấy ảnh từ nguồn ...";
+                Thread thread = new Thread(delegate ()
+                {
+                    int i = 0;
+                    int complete = 0;
+                ChangeAPI:
+                    RestClient restClient = new RestClient(string.Concat(new object[]
+                    {
+                    "https://api.unsplash.com/photos/random?",
+                    topic,
+                    "count=",
+                    30,
+                    "&client_id="+api[key]
+                    }));
+                    RestRequest request = new RestRequest(Method.GET);
+                    response = restClient.Execute(request);
+                    if (response.IsSuccessful)
+                    {
+                        RadForm1.RootObject[] result = JsonConvert.DeserializeObject<RadForm1.RootObject[]>(response.Content);
+                        string[] collection = File.ReadAllLines("listid.txt");
+                        List<string> list = new List<string>();
+                        list.AddRange(collection);
+                        foreach (RadForm1.RootObject rootObject in result)
+                        {
+                            if (!list.Contains(rootObject.id))
+                            {
+                                using (StreamWriter streamWriter = new StreamWriter("listid.txt", true))
+                                {
+                                    streamWriter.WriteLine(rootObject.id);
+                                }
+                            }
+                        }
+                        collection = File.ReadAllLines("listid.txt");
+                        this.lblStatus.Text = string.Concat(new object[]
+                        {
+                        "Lấy được ",
+                        result.Count<RadForm1.RootObject>(),
+                        " /",
+                        this.txtNum.Text,
+                        " ảnh không bị trùng"
+                        });
+
+                        RadForm1.RootObject[] result2 = result;
+                        for (int j = 0; j < result2.Length; j++)
+                        {
+                            RadForm1.RootObject obj = result2[j];
+                            using (WebClient webClient = new WebClient())
+                            {
+                                Uri myUri = new Uri(obj.urls.regular, UriKind.Absolute);
+                                webClient.DownloadFileAsync(myUri, this.path + "//Tata-" + obj.id + ".jpg");
+                                while (webClient.IsBusy)
+                                {
+                                    Application.DoEvents();
+                                }
+                            }
+                            int i2 = i;
+                            i = i2 + 1;
+                            this.lblStatus.Text = string.Concat(new object[]
+                            {
+                                "Tải về ",
+                                i,
+                                " /",
+                                num,
+                                " ảnh"
+                            });
+                        }
+                        complete = complete + result2.Length;
+                        if (complete > num)
+                            return;
+                        else
+                            goto ChangeAPI;
+
+                    }
+                    key++;
+                    goto ChangeAPI;
+                })
+                {
+                    IsBackground = true
+                };
+                thread.Start();
+                while (thread.IsAlive)
+                {
+                    Application.DoEvents();
+                    this.btnStart.Enabled = false;
+                }
+                btnStart.Enabled = true;
+            }
+            else
+                MessageBox.Show("Số lượng hình không đúng định dạng.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btn_SetFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    this.path = folderBrowserDialog.SelectedPath;
+                    Settings.Default.path = folderBrowserDialog.SelectedPath;
+                    Settings.Default.Save();
+                    this.lblStatus.Text = "Thư mục đang lưu: " + this.path;
+                    MessageBox.Show("Đã chọn thư mục lưu. Hệ thống tự lưu lại lần kế tiếp.");
+                }
+            }
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("key");
+            dataTable.Columns.Add("name");
+            dataTable.Rows.Add(new object[]
+            {
+                "",
+                "Không chủ đề"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=architecture&",
+                "Kiến trúc"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=sport&",
+                "Thể thao"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=travel&",
+                "Du lịch"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=beauty&",
+                "Làm đẹp"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=fashion&",
+                "Thời trang"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=pet&",
+                "Vật nuôi"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=art&",
+                "Nghệ thuật"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=photography&",
+                "Nhiếp ảnh"
+            });
+            dataTable.Rows.Add(new object[]
+            {
+                "query=food&",
+                "Ẩm thực"
+            });
+            if (Properties.Settings.Default.path != null)
+                this.path = Properties.Settings.Default.path;
+            this.ckbTopic.DataSource = dataTable;
+            this.ckbTopic.ValueMember = "key";
+            this.ckbTopic.DisplayMember = "name";
+            this.lblStatus.Text = "Thư mục đang lưu: " + this.path;
+        }
+        public class Urls
+        {
+            public string raw { get; set; }
+            public string full { get; set; }
+            public string regular { get; set; }
+            public string small { get; set; }
+            public string thumb { get; set; }
+        }
+        public class RootObject
+        {
+            public string id { get; set; }
+            public RadForm1.Urls urls { get; set; }
         }
     }
 }
